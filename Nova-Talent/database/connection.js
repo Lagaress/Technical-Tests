198d0c70f1d61e578347d53c3a2df2768be6c6dd
Jesús Lagares <48330849+Lagaress@users.noreply.github.com>
2022-07-04 22:07:01 +0200

    Config details outsorced to config file

--- Nova-Talent/database/connection.js
+++ Nova-Talent/database/connection.js
@@ -1,36 +1,45 @@
-const { connect } = require('http2');
-var mysql = require('mysql'); 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "localhost",
-    password: "root"
+const mysql = require('mysql')
+const config = require('./config.json')
+const connection = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password
 });
 
-connection.connect(function(err) 
+function connectDatabase()
 {
-    try 
+    connection.connect(function(err) 
     {
-        establishConnection(err) ;
-    }
-    catch
-    {
-        console.log("Some error have ocurred trying to establish a connection with database, please check the log file");
-    }
-    
-});
+        try 
+        {
+            establishConnection(err) 
+            return connection
+        }
+        catch
+        {
+            console.log("Some error have ocurred trying to establish a connection with database, please check the log file")
+        }
+        
+    })
+}
+
+function closeDatabase()
+{
+    connection.end() 
+}
 
 function establishConnection(err)
 {
     if (err) 
     {
-        throw err;
+        throw err
     }    
     console.log("Connected!");
     connection.query("use nova" , function(err)
     {
         if (err && err.code == 'ER_BAD_DB_ERROR')
         {
-            createDatabase() ;
+            createDatabase() 
         }
     });
 }
@@ -39,18 +48,25 @@ function createDatabase()
 {
     connection.query("CREATE DATABASE nova" , function(err)
     {
-        if (err) throw err ; 
-        console.log("Database nova created successfully!") ;
-        connection.query("use nova") ;
-        createNominationTable() ;
-    }) ;
+        if (err) throw err 
+        console.log("Database nova created successfully!") 
+        connection.query("use nova") 
+        createNominationTable() 
+    }) 
 }
 
 function createNominationTable()
 {
-    var createNominationTable = "CREATE TABLE IF NOT EXISTS nominations (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(50), explanation VARCHAR(255), involvement INT, overall INT, accepted BOOL)";
+    var createNominationTable = "CREATE TABLE IF NOT EXISTS nominations (id INT AUTO_INCREMENT PRIMARY KEY, userWhoNominate VARCHAR(50),  email VARCHAR(50), explanation VARCHAR(255), involvement INT, overall INT, accepted BOOL)";
     connection.query(createNominationTable, function (err) {
-        if (err) throw err;
-        console.log("Table created successfully");
-    });
+        if (err) throw err
+        console.log("Table created successfully")
+    })
+}
+
+module.exports =
+{
+
+    connectDatabase,
+    closeDatabase
 }
