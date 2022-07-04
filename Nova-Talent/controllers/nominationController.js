c814f238e3cc01a54aeb5f39c4fa32897b0a6d36
Jesús Lagares <48330849+Lagaress@users.noreply.github.com>
2022-07-04 21:21:24 +0200

    Controllers basic use outlined

---
+++ Nova-Talent/controllers/nominationController.js
@@ -0,0 +1,24 @@
+const Nomination = require('../models/nominationModel')
+
+function addNomination(req, res)
+{
+
+}
+
+function showNominationList(req , res)
+{
+
+
+}
+
+function showInstructions(req, res)
+{
+
+}
+
+module.exports = 
+{
+    addNomination,
+    showNominationList,
+    showInstructions
+}
--- Nova-Talent/server.js
+++ Nova-Talent/server.js
@@ -1,27 +1,31 @@
-const http = require('http');
+const http = require('http')
+const {addNomination , showNominationList , showInstructions} = require('./controllers/nominationController')
 const server = http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-type": "text/plain" });
+    res.writeHead(200, { "Content-type": "text/plain" })
     manageURLs(req , res) ;
-});
+})
 
 server.listen(8000, function() {
     console.log('Server is running at 8000')
-});
+})
 
 function manageURLs(req , res)
 {
     if (req.url == '/nominate' && req.method == 'POST')
     {
-        res.end("Now you can nominate") ;
+        res.end("Now you can nominate") 
+        addNomination(req , res)
     }
 
     else if (req.url == '/nominations-list' && req.method == 'GET')
     {
-        res.end("Here is where you're going to see all nominations") ;
+        res.end("Here is where you're going to see all nominations") 
+        showNominationList(req , res)
     }
 
     else
     {
-        res.end("API instructions") ;
+        res.end("API instructions") 
+        showInstructions(req , res)
     }
 }
