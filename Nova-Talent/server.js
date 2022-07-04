39fd12d51d9f258e27fabda4401663136ca60b0d
Jesús Lagares <48330849+Lagaress@users.noreply.github.com>
2022-07-04 17:10:07 +0200

    Routing management added

--- Nova-Talent/server.js
+++ Nova-Talent/server.js
@@ -1,10 +1,27 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
+const http = require('http');
+const server = http.createServer(function(req, res) {
     res.writeHead(200, { "Content-type": "text/plain" });
-    res.end("Welcome to the Nova Talent API\n");
+    manageURLs(req , res) ;
 });
 
 server.listen(8000, function() {
     console.log('Server is running at 8000')
 });
+
+function manageURLs(req , res)
+{
+    if (req.url == '/nominate' && req.method == 'POST')
+    {
+        res.end("Now you can nominate") ;
+    }
+
+    else if (req.url == '/nominations-list' && req.method == 'GET')
+    {
+        res.end("Here is where you're going to see all nominations") ;
+    }
+
+    else
+    {
+        res.end("API instructions") ;
+    }
+}
