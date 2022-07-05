7b59e4a70e80233dcb36269ee247bd3d5ccb1f59
Jesús Lagares <48330849+Lagaress@users.noreply.github.com>
2022-07-06 00:25:23 +0200

    nominationController.js refactored

--- Nova-Talent/controllers/nominationController.js
+++ Nova-Talent/controllers/nominationController.js
@@ -1,35 +1,41 @@
-var NominationModel = require('../models/nominationModel')
+const NominationModel = require('../models/nominationModel')
 
 async function addNomination(res, Nomination)
 {
     try
     {
-        var flag = await NominationModel.addNominationToDb(Nomination)
+        const isAddingNominationPossibly = await NominationModel.addNominationToDb(Nomination)
         res.writeHead(201 , {'Content-Type': 'application/json'})
-        if (flag)
-        {
-            res.write("Nomination added successfully");
-        }
-        else
-        {
-            res.write("The user that you're trying to add is already on database");
-        }
+        res.write(distinguishNominationCases(isAddingNominationPossibly))
         res.end()
     }
     catch (err)
     {
+        //TODO: ESCRIBIR EN EL ARCHIVO DE LOGS
         console.log(err)
     }
 
+}
+
+function distinguishNominationCases(isAddingNominationPossibly)
+{
+    if (isAddingNominationPossibly)
+    {
+        return "Nomination added successfully"
+    }
+    else
+    {
+        return "The user that you're trying to add is already on database"
+    }
 }
 
 async function showNominationList(res)
 {
     try
     {
-        var nominationList = await NominationModel.getNominationsNonRejected()
+        const nominationList = await NominationModel.getNominationsNonRejected()
         res.writeHead(200 , {'Content-Type': 'application/json'})
-        res.write(distinguishEmptyList(nominationList))
+        res.write(distinguishEmptyListCases(nominationList))
         res.end()
     }
     catch (err)
@@ -39,7 +45,7 @@ async function showNominationList(res)
     }
 }
 
-function distinguishEmptyList(nominationList)
+function distinguishEmptyListCases(nominationList)
 {
     if (nominationList == [])
     {
@@ -53,7 +59,7 @@ function distinguishEmptyList(nominationList)
 
 function showInstructions(req, res)
 {
-
+    //TOO - DO
 }
 
 module.exports = 
